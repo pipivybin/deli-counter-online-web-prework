ba832d781a4c473f11e46adb0b6e84751ef514ed
Ming Duan <11203duan@gmail.com>
2019-02-02 22:08:18 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,25 @@
-# Write your code here.
+def line(array)
+  if array.empty? == true
+    puts "The line is currently empty."
+  else
+    arr = ["The line is currently:"]
+    array.each_with_index {|name, x| arr << " #{x+1}. #{name}"}
+    puts arr.join
+  end
+end
+
+def take_a_number(array,name)
+  num = array.length
+  puts "Welcome, #{name}. You are number #{num+1} in line."
+  array << name
+end
+
+def now_serving(arr)
+  if arr.empty?
+    puts "There is nobody waiting to be served!"
+  else
+    name = arr[0]
+    puts "Currently serving #{name}."
+    arr.shift
+end
+end
