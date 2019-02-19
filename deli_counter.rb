611c2af351851c1f4d144f086dbbd0cfb747cf76
Ming Duan <11203duan@gmail.com>
2019-02-19 17:53:45 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -15,5 +15,5 @@ def take_a_number(array,name)
 end
 
 def now_serving(arr)
-  arr.empty?? (puts "There is nobody waiting to be served!") : (puts "Currently serving #{arr}.shift.")
+  arr.empty? ? (puts "There is nobody waiting to be served!") : (puts "Currently serving #{arr}.shift.")
 end
